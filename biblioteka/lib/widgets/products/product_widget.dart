1c266c8cec306f87a188a84e340d786099a82d08
anastasijaisaku <isaku.anastasija7@gmail.com>
2026-01-16 02:12:30 +0100

    Widget auth

---
+++ biblioteka/lib/widgets/products/product_widget.dart
@@ -0,0 +1,103 @@
+
+import 'package:fancy_shimmer_image/fancy_shimmer_image.dart';
+import 'package:flutter/material.dart';
+import 'package:skriptarnicaftn/consts/app.colors.dart';
+import 'package:skriptarnicaftn/consts/app_constants.dart';
+import 'package:skriptarnicaftn/screens/inner_screen/product_details.dart';
+import 'package:skriptarnicaftn/widgets/products/heart_btn.dart';
+import 'package:skriptarnicaftn/widgets/subtitle_text.dart';
+import 'package:skriptarnicaftn/widgets/title_text.dart';
+
+class ProductWidget extends StatefulWidget {
+  const ProductWidget({super.key});
+
+  @override
+  State<ProductWidget> createState() => _ProductWidgetState();
+}
+
+class _ProductWidgetState extends State<ProductWidget> {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Padding(
+      padding: const EdgeInsets.all(0.0),
+      child: GestureDetector(
+        onTap: () async {
+            Navigator.pushNamed(context, ProductDetailsScreen.routName);
+          },
+        child: Column(
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(12.0),
+              child: FancyShimmerImage(
+                imageUrl: AppConstants.imageUrl,
+                height: size.height * 0.22,
+                width: double.infinity,
+              ),
+            ),
+            const SizedBox(
+              height: 12.0,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(2.0),
+              child: Row(
+                children: [
+                  Flexible(
+                    flex: 5,
+                    child: TitelesTextWidget(
+                      label: "Title " * 10,
+                      fontSize: 18,
+                      maxLines: 2,
+                    ),
+                  ),
+                  const Flexible(
+                    flex: 2,
+                    child: HeartButtonWidget()
+                    
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 6.0,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(2.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Flexible(
+                    flex: 1,
+                    child: SubtitleTextWidget(
+                      label: "1200 RSD",
+                      fontWeight: FontWeight.w600,
+                      color: AppColors.darkPrimary,
+                    ),
+                  ),
+                  Flexible(
+                    child: Material(
+                      borderRadius: BorderRadius.circular(12.0),
+                      color: AppColors.lightPrimary,
+                      child: InkWell(
+                        borderRadius: BorderRadius.circular(12.0),
+                        onTap: () {},
+                        splashColor: Colors.blueGrey,
+                        child: const Padding(
+                          padding: EdgeInsets.all(6.0),
+                          child: Icon(Icons.add_shopping_cart_outlined),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 12.0,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
